e497c92cd6efa1c8367b8a820fdcd5ef4b5f6c9e
Serge Poltavski <serge.poltavski@gmail.com>
2017-10-14 00:32:39 +0300

    win: new icon set

--- ceammc/gui/icons/make_icons.sh
+++ ceammc/gui/icons/make_icons.sh
@@ -2,6 +2,7 @@
 
 INK='@INKSCAPE@'
 ICONSET='@CMAKE_CURRENT_SOURCE_DIR@/pd_ceammc.iconset'
+ICONSET_WIN='@CMAKE_CURRENT_SOURCE_DIR@/pd_ceammc_win.iconset'
 
 if [ -z "$1" ]
 then
@@ -10,6 +11,8 @@ then
 fi
 
 mkdir -p ${ICONSET}
+rm -rf ${ICONSET_WIN}
+mkdir -p ${ICONSET_WIN}
 
 BASE=${ICONSET}/icon
 SVG="`pwd`/$1"
@@ -21,13 +24,26 @@ make_icon() {
     $INK -z -D -e "${BASE}_${sz}x${sz}@2x.png" -f $SVG -w ${sz2} -h ${sz2}
 }
 
+make_win_icon() {
+    svg=$1
+    sz=$2
+    $INK -z -C -e "${ICONSET_WIN}/icon_${sz}x${sz}.png" -f `pwd`/$svg -w ${sz} -h ${sz}
+}
+
 make_icon 16
 make_icon 32
 make_icon 128
 make_icon 256
 make_icon 512
+make_win_icon pd_ceammc_win_16.svg 16
+make_win_icon pd_ceammc_win_32.svg 32
+make_win_icon pd_ceammc_win_48.svg 48
+make_win_icon pd_ceammc_win_64.svg 64
+make_win_icon pd_ceammc_win_48.svg 96
+make_win_icon pd_ceammc_win_64.svg 256
 
 cp ${ICONSET}/icon_512x512.png ./puredata-ceammc.png
+cp ${ICONSET_WIN}/icon_256x256.png ./puredata-ceammc_win.png
 
 if [ -e "@ICONUTIL@" ]
 then
@@ -41,4 +57,7 @@ then
     echo "Making ICO..."
     PNGS=$(find "${ICONSET}" -name '*.png' | grep -v '@')
     @MAGICK_CONVERT@ ${PNGS} pd_ceammc.ico
+
+    PNGS=$(find "${ICONSET_WIN}" -name '*.png')
+    @MAGICK_CONVERT@ ${PNGS} pd_ceammc_win.ico
 fi
